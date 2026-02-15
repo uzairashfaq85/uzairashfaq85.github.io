b334b28ed80e957a59484e765dc8e424aed04452
uzaireeyy <uzairashfaq85@gmail.com>
2026-02-15 22:36:18 +0100

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,27 +1,20 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./Footer.scss";
-import {Fade} from "react-reveal";
-import emoji from "react-easy-emoji";
+import { Fade } from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
-  const {isDark} = useContext(StyleContext);
+  const { isDark } = useContext(StyleContext);
+
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
+
+        {/* Your custom footer */}
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
+          © {new Date().getFullYear()} Uzair Ashfaq. All rights reserved.
         </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
-          <a
-            href="https://github.com/saadpasta/developerFolio"
-            target="_blank"
-            rel="noreferrer"
-          >
-            developerFolio
-          </a>
-        </p>
+
       </div>
     </Fade>
   );
